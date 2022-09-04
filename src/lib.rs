99e8653107e6545a3441ab55a42d10e9da9ae4de
w02nadd <aruiskyhappy2@outlook.com>
2022-09-04 23:45:45 +0800

    Create more enemies and bugs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
-pub mod creates;
+//pub mod creates;
 
-pub use crate::creates::create_characters;
-pub use crate::creates::create_enemies;
+//pub use crate::creates::create_characters;
+//pub use crate::creates::create_enemies;
 
 
-fn creates() {
-    create_characters::create_atri;
-    create_characters::create_tifa
-    create_enemies::create_monsters
-}
+//fn creates() {
+    //create_characters::create_atri;
+    //create_characters::create_tifa
+    //create_enemies::create_monsters
+//}
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 use std::{io};
 use std::cmp::Ordering; 
-struct BattleCharacter {
+struct Atri {
     name: String,
     health: i32,
     normal_attack: i32,
@@ -10,13 +10,14 @@ struct BattleCharacter {
     recovery: i32,
 }
 
-impl BattleCharacter{
+impl Atri {
+
     fn output(&self) {       //A debug tool
-        let healthy = &self.health;
-        let attacky = &self.normal_attack;
-        let defencey = &self.defence;
-        let recoveryy = &self.recovery;
-        println!("Your character is {},{},{},{}",healthy,attacky,defencey,recoveryy);
+        println!("Your character is {},{},{},{}",
+        &self.health,
+        &self.normal_attack,
+        &self.defence,
+        &self.recovery);
     }
 
     fn rocket_fists(&self)-> i32 {
@@ -24,85 +25,119 @@ impl BattleCharacter{
     }
 }
 
-struct Enemy{
+struct Tifa {
+    name: String,
+    health: i32,
+    normal_attack: i32,
+    defence: i32,
+    recovery: i32,
+}
+
+impl Tifa {
+
+    fn output(&self) {       //A debug tool
+        println!("Your character is {},{},{},{}",
+        &self.health,
+        &self.normal_attack,
+        &self.defence,
+        &self.recovery);
+    }
+}
+struct Monk{
     health: i32,
     normal_attack: i32,
     defence: i32,
 }
 
-impl Enemy{
-    fn output(&self) {       //A debug tool
-        let healthy = &self.health;
-        let attacky = &self.normal_attack;
-        let defencey = &self.defence;
-        println!("Your character is {},{},{}",healthy,attacky,defencey);
+impl Monk{
+
+    fn new()-> Self {
+        Monk { 
+            health: 300,
+            normal_attack: 40,
+            defence: 5
+        }
     }
 
-    fn rain(&self) -> i32 {
-        14
+    fn output(&self) {
+        println!("Your enemy is Monk,{},{},{}", &self.health, &self.normal_attack, &self.defence);
     }
 }   
 
+struct Shaman {
+    health: i32,
+    normal_attack: i32,
+    defence: i32,
+    recovery: i32
+}
+
+struct Challenger {
+    health: i32,
+    normal_attack: i32,
+    defence: i32,
+}
+
+struct Expect {
+    
+}
 
 fn main() {
     println!("Please choose your character: Atri/Tifa");
+
     let mut input = String::new();
 
     io::stdin()         //process user's inputs
         .read_line(&mut input)
         .expect("Fail to read");
-
-    let mut character = if input.trim() == "atri" {
+    
+    let mut character = if input.trim() == "atri" || input.trim() == "Atri" {
         create_atri()
     }else {
         create_tifa()
     };
     
-    let mut monsters = create_monsters(); 
-    character.output();
-    monsters.output();
+    let mut enemies = Monk::new(); 
+    enemies.output();
 
-    while character.health > 0 && monsters.health > 0 {         //main process part
-        let mut i = 1;
-        while i % 2 == 1 {
-            let mut user_input = String::new();
+    while character.health > 0 && enemies.health > 0 {         //main process part
 
-            println!("It's your turn");
-            if character.name == String::from("atri") {
-                println!("You can use:\n1.normal attack\n2.rocket fists");
-            }else {
-                println!("You can use:\n1.normal attack");
+        let mut user_input = String::new();
+
+        println!("It's your turn");
+
+        if character.name == String::from("atri") {
+            println!("You can use:\n1.normal attack\n2.rocket fists");
+        }else {
+            println!("You can use:\n1.normal attack");
+        }
+
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("Fail to read");
+
+        if character.name == String::from("atri") {
+            if user_input.trim() == "1" || user_input.trim() == "normal attack" {
+                enemies.health -= character.normal_attack - enemies.defence;
+            }else if user_input.trim() == "2" || user_input.trim() == "rocket fists"{
+                enemies.health -= character.rocket_fists() - enemies.defence;
             }
 
-            io::stdin()
-                .read_line(&mut user_input)
-                .expect("Fail to read");
+            println!("The monster remains:{}",enemies.health);
 
-            if character.name == String::from("atri") {
-                if user_input.trim() == "1" || user_input.trim() == "normal attack" {
-                    monsters.health -= character.normal_attack - monsters.defence;
-                }else if user_input.trim() == "2" || user_input.trim() == "rocket fists"{
-                    monsters.health -= character.rocket_fists() - monsters.defence;
-                }
-
-                println!("The monster remains:{}",monsters.health);
-
-            }else {
-                if user_input.trim() == "1" || user_input.trim() == "normal attack" {
-                    monsters.health -= character.normal_attack - monsters.defence;
-                }
-
-                println!("The monster remains:{}",monsters.health);
+        }else {
+            if user_input.trim() == "1" || user_input.trim() == "normal attack" {
+                enemies.health -= character.normal_attack - enemies.defence;
             }
 
-            character.health -= monsters.normal_attack - character.defence;
-            println!("You remains:{}",character.health);
+            println!("The monster remains:{}",enemies.health);
+        }
 
-            i += 1;
-        } 
+        character.health -= enemies.normal_attack - character.defence;
+        println!("You remains:{}",character.health);
+
     }
 
-    match character.health.cmp(&monsters.health) {
+    match character.health.cmp(&enemies.health) {
         Ordering::Greater => println!("The monster has been beaten.YOU WIN!"),
         Ordering::Less => println!("YOU has been beaten.YOU LOSE!"),
         Ordering::Equal => println!("BOOMMMMM!!!!,nobody win"),
@@ -111,36 +146,30 @@ fn main() {
     
 }
 
-
-
-fn create_atri()-> BattleCharacter {         //Create a atri
-    BattleCharacter { 
+fn create_atri() -> Atri {
+    Atri { 
         name: String::from("atri"),
         health: 100, 
         normal_attack: 100, 
         defence: 10, 
-        recovery: 50
+        recovery: 50 
     }
 }
 
-fn create_tifa()-> BattleCharacter {        //Create a tifa
-    BattleCharacter { 
+fn create_tifa() -> Tifa {
+    Tifa { 
         name: String::from("tifa"),
         health: 130, 
         normal_attack: 70, 
         defence: 15, 
-        recovery: 40 
+        recovery: 40
     }
 }
 
-fn create_monk()-> Enemy {
-    Enemy { 
-        health: 300,
-        normal_attack: 40,
-        defence: 5
+fn create_monk() -> Monk {
+    Monk { 
+        health: 130, 
+        normal_attack: 70, 
+        defence: 15, 
     }
-}
-
-fn create_shaman()-> Enemy {
-    
 }
